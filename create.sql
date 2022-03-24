ba9a5cd17bf113df7e27db8ce95e1eff6b368fca
Eliana Cleovoulou <eliana_97@live.com>
2022-03-24 17:28:37 +0100

    add column data constraints and trigger for the commited submission

--- create.sql
+++ create.sql
@@ -1,22 +1,29 @@
+DROP TABLE IF EXISTS Student;
+CREATE TABLE Student(
+  st_name VARCHAR(50) NOT NULL, 
+  email VARCHAR(50) UNIQUE NOT NULL,
+  universityid INTEGER NOT NULL,
+  PRIMARY KEY (universityID));
+  
 DROP TABLE if exists Question;
 CREATE TABLE Question(
   title VARCHAR(50) NOT NULL,
   content VARCHAR(50) NOT NULL,
-  questionid INTEGER, 
-  PRIMARY KEY (questionid));
+  questionid INTEGER PRIMARY KEY AUTOINCREMENT 
+  );
 
 DROP TABLE if exists Task;
 CREATE TABLE Task(
   title VARCHAR(50) NOT NULL,
   content VARCHAR(50) NOT NULL,
   questionid INTEGER NOT NULL, 
-  taskid INTEGER NOT NULL,
-  PRIMARY KEY (taskid));
+  taskid INTEGER PRIMARY KEY AUTOINCREMENT
+  );
   
 DROP TABLE IF EXISTS Task_question;
 CREATE TABLE Task_question(
   taskid INTEGER NOT NULL,
-  questionid INTEGER NOT NULL,
+  questionid INTEGER NOT NULL ,
   FOREIGN KEY (taskid) REFERENCES Task ON UPDATE RESTRICT ON DELETE RESTRICT,
   FOREIGN KEY (questionid) REFERENCES Question ON UPDATE RESTRICT ON DELETE RESTRICT,
   PRIMARY KEY (taskid, questionid));
@@ -25,29 +32,23 @@ DROP TABLE IF EXISTS Assignment;
 CREATE TABLE Assignment(
   universityid INTEGER NOT NULL,
   taskid INTEGER NOT NULL,
-  assignmentid INTEGER NOT NULL,
-  FOREIGN KEY (taskid) REFERENCES Task, 
-  PRIMARY KEY (assignmentid)); 
-  
+  assignmentid INTEGER PRIMARY KEY AUTOINCREMENT,
+  FOREIGN KEY (taskid) REFERENCES Task ON UPDATE RESTRICT ON DELETE RESTRICT, 
+  FOREIGN KEY (universityid) REFERENCES Student ON UPDATE RESTRICT ON DELETE RESTRICT
+  ); 
+  -- i am not sure if the trigger has the option of print, couldnt find anything about it 
 CREATE TRIGGER IF NOT EXISTS NEW_ASSIGNMENT
 AFTER INSERT ON Assignment
 BEGIN 
 	PRINT('New assignment has been added') 
 END;
 
-DROP TABLE IF EXISTS Student;
-CREATE TABLE Student(
-  name VARCHAR(50), 
-  email VARCHAR(50),
-  universityid INTEGER NOT NULL,
-  PRIMARY KEY (universityID));
-  
+
 DROP TABLE IF EXISTS Submission; 
 CREATE TABLE Submission(
   assignmentid INTEGER NOT NULL,
-  submissionid INTEGER NOT NULL, 
-  PRIMARY KEY (submissionid), 
-  FOREIGN KEY (assignmentid) REFERENCES Assignment); 
+  submissionid INTEGER PRIMARY KEY AUTOINCREMENT, 
+  FOREIGN KEY (assignmentid) REFERENCES Assignment ON UPDATE RESTRICT ON DELETE RESTRICT); 
   
 CREATE TRIGGER IF NOT EXISTS NEW_SUBMISSION
 AFTER INSERT ON Submission
@@ -60,45 +61,51 @@ CREATE TABLE Answers(
   content VARCHAR(50), 
   questionid INTEGER NOT NULL,
   submissionid INTEGER NOT NULL, 
-  answerid INTEGER NOT NULL, 
-  Primary KEY (answerid), 
-  FOREIGN KEY (questionid) REFERENCES Question,
-  FOREIGN KEY (submissionid) REFERENCES Submission);
+  answerid INTEGER PRIMARY KEY AUTOINCREMENT, 
+  FOREIGN KEY (questionid) REFERENCES Question ON UPDATE RESTRICT ON DELETE RESTRICT,
+  FOREIGN KEY (submissionid) REFERENCES Submission ON UPDATE RESTRICT ON DELETE RESTRICT);
 
+-- doesnt make sense to allow updates on sumbsission while there is an evaluation request (I chenge it to restrict)
 DROP TABLE IF EXISTS EvaluationRequest;
 CREATE TABLE EvaluationRequest(
   submissionid INTEGER NOT NULL, 
-  requestid INTEGER NOT NULL,
-  PRIMARY KEY (requestid),
-  FOREIGN KEY (submissionid) REFERENCES Submission ON UPDATE NO ACTION);
+  requestid INTEGER PRIMARY KEY AUTOINCREMENT,
+  FOREIGN KEY (submissionid) REFERENCES Submission ON UPDATE RESTRICT);
 
 -- Make sure that the ON UPDATE NO ACTION can be added on scores
 -- or make a INSTEAD OF trigger so the table scores isn't changed
 DROP TABLE IF EXISTS Evaluation;
 CREATE TABLE Evaluation(
   evaluationid INTEGER NOT NULL,
-  requestid INTEGER NOT NULL, 
-  PRIMARY KEY (evaluationid));
+  requestid INTEGER PRIMARY KEY AUTOINCREMENT
+  );
 
 DROP TABLE IF EXISTS Score;
 CREATE TABLE Score(
   value INTEGER NOT NULL CHECK (value BETWEEN 1 AND 10), 
-  scoreid INTEGER NOT NULL,
+  scoreid INTEGER PRIMARY KEY AUTOINCREMENT,
   answerid INTEGER NOT NULL, 
   evaluationid INTEGER NOT NULL, 
-  PRIMARY KEY (scoreid), 
-  FOREIGN KEY (answerid) REFERENCES Answers, 
-  FOREIGN KEY (evaluationid) REFERENCES Evaluation);
+  FOREIGN KEY (answerid) REFERENCES , 
+  FOREIGN KEY (evaluationid) REFERENCES);
 
 DROP TABLE IF EXISTS EvaluationFinished;
 CREATE TABLE EvaluationFinished(
-  finishedid INTEGER NOT NULL, 
+  finishedid INTEGER PRIMARY KEY AUTOINCREMENT, 
   evaluationid INTEGER NOT NULL,
-  PRIMARY KEY (finishedid), 
   FOREIGN KEY (evaluationid) REFERENCES Evaluation);
 
 CREATE TRIGGER IF NOT EXISTS SUBMISSION_EVALUATED
 AFTER INSERT ON EvaluationFinished
 BEGIN 
 	PRINT('Your submission has been evaluated') 
+END;
+
+CREATE TRIGGER IF NOT EXISTS COMMITED_SUBMISSION
+BEFORE INSERT on Answers
+BEGIN 
+SELECT CASE
+WHEN NOT((SELECT submissionid FROM EvaluationRequest WHERE submissionid == NEW.submissionid) ISNULL)
+THEN RAISE(ABORT, 'Cannot add answer to a submission which is commited')
+END;
 END;
