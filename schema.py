6f99d6cdd2acdfc0dd9d4a23f2465a521cd97e4a
Marios Christoforides <marchri99@gmail.com>
2022-04-06 23:45:09 +0200

    corrections

--- schema.py
+++ schema.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 
 base = declarative_base()
 class Task(base):
-    _tablename_ = "Task"
+   __tablename__ = "Task"
     taskid = Column(Integer,primary_key=True)
     title = Column(String(50))
     content = Column(String(50))
@@ -15,47 +15,48 @@ class Task(base):
     task_q_r = relationship("Task_question")
     
 class Assignment(base):
-    _tablename_ = "Assignment"
+    __tablename__ = "Assignment"
     assignmentid = Column(Integer,primary_key=True)
     universityid = Column(ForeignKey('Student.universityid'))
     taskid = Column(ForeignKey('Task.taskid'), nullable=True)
     submission_r = relationship("Submission")
     
 class Task_question(base):
-    _tablename_ = "Task_question"
+    __tablename__ = "Task_question"
+    id(Integer, primary_key = True)
     taskid = Column(ForeignKey('Task.taskid'))
     questionid = Column(ForeignKey('Question.questionid'), nullable=True)
 
 class Student(base):
-    _tablename_ = "Student"
+    __tablename__ = "Student"
     universityid = Column(Integer,primary_key=True)
     name = Column(String)
     email = Column(String)
     sttudent_r = relationship("Assignment")
     
 class Submission(base):
-    _tablename_ = "Submission"
+    __tablename__ = "Submission"
     submissionid = Column(Integer,primary_key=True)
     assignmentid = Column(ForeignKey('Assignment.assignmentid'), nullable=True)
     eval_r = relationship("EvaluationRequest")
     answers_r = relationship("Answers")
     
 class EvaluationRequest(base):
-    _tablename_ = "EvaluationRequest"
+    __tablename__ = "EvaluationRequest"
     requestid = Column(Integer,primary_key=True)
     submissionid = Column(ForeignKey('Submission.submissionid'), nullable=True)
     eval_r = relationship("Evaluation")
     
 class Question(base):
-    _tablename_ = "Question"
+    __tablename__ = "Question"
     questionid = Column(Integer,primary_key=True)
     title = Column(String(50))
     content = Column(String(50))
     task_q_r = relationship("Task_question")
-    answer_r = relationship("Answer")
+    answer_r = relationship("Answers")
     
 class Answers(base):
-    _tablename_ = "Answers"
+    __tablename__ = "Answers"
     answerid = Column(Integer,primary_key=True)
     content = Column(String(1000))
     questionId = Column(ForeignKey('Question.questionid'), nullable=True)
@@ -63,20 +64,20 @@ class Answers(base):
     scores_r = relationship("Scores")
     
 class Evaluation(base):
-    _tablename_ = "Evaluation"
+    __tablename__ = "Evaluation"
     evaluationid = Column(Integer, primary_key = True)
     requestid = Column(ForeignKey('EvaluationRequest.requestid'), nullable=True)
     evaluation_f_r = relationship("EvaluationFinished")
     scores_r = relationship("Scores")
 
 class Scores(base):
-    _tablename_ = "Scores"
+    __tablename__ = "Scores"
     scoreid = Column(Integer, primary_key = True)
     value = Column(Integer)
     answerid = Column(ForeignKey('Answers.answerid'),nullable = True)
     evaluationid = Column(ForeignKey('Evaluation.evaluationid'))
     
 class EvaluationFinished(base):
-    _tablename_ = "EvaluationFinished"
+    __tablename__ = "EvaluationFinished"
     finishedid = Column(Integer, primary_key = True)
     evaluationid = Column(ForeignKey('Evaluation.evaluationid'),nullable = True)
