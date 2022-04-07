ccc062547ff9bd07b2f408cba78dc1a48928e7d3
Eliana Cleovoulou <eliana_97@live.com>
2022-04-07 23:46:14 +0200

    fix if statements, debug gradedb.py

--- gradedb.py
+++ gradedb.py
@@ -33,62 +33,70 @@ class Gradedb:
             raise Exception("Content must be provided")
         else:
             with self.newSession() as ses:
-                q = Question(Title = title, Text = content)
+                q = Question(title = title, content = content)
                 ses.add(q)
                 ses.commit()
                 return q.questionid
 
 
-    def addTask(self, questionid, title, content):
-        ''''The teacher adds tasks by providing the questionid, title and content (input of addTask function).'''
+    def addTask(self, questionids, title, content):
+        ''''The teacher adds tasks by providing ta list with the questionids, title and content (input of addTask function).'''
         with self.newSession() as ses:
-            t = Task(Questions = questionid, Title = title, Text = content)
+            t = Task(title = title, content = content)
             ses.add(t)
-            ses.commit()
+            ses.commit() #commit to create the taskid 
+            for i in questionids: #iterating over each questionid
+                task_question = Task_question(questionid = i, taskid = t.taskid)
+                ses.add(task_question)
+                ses.commit()
             return t.taskid
     
     def addAssignment(self, universityid, taskid ):
         '''The teacher adds assignment by providing the universityid, which is the studentid and taskid (input of addAssignemnt function).
         It checks if the tasks have questions, if not raises an exception.'''
-        if select(Task).where(Task.questionid) == NULL:
+        query = select(Task_question).where(Task_question.taskid == taskid)
+        result = self._engine.execute(query)
+        if len(result.all()) == 0:
             raise exception('Does not make sence to add assignement of a task with no questions')
         else:
             with self.newSession() as ses:
-                a = Assignment(Student = universityid, Task = taskid)
+                a = Assignment(universityid = universityid, taskid = taskid)
                 ses.add(a)
                 ses.commit()
                 print("New assignment added")
                 return a.assignmentid
     
     def newSubmission(self,assignmentid):
-        ''''The teacher adds new submission by providing the assignmentid (input of the newsubmission function).'''
+        ''''A student adds new submission by providing the assignmentid (input of the newsubmission function).'''
         with self.newSession() as ses:
-            s = Submission(Assignment = assignmentid)
+            s = Submission(assignmentid = assignmentid)
             ses.add(s)
             ses.commit()
             return s.submissionid
     
     def addAnswer(self, content, questionid, submissionid):
-        '''The teacher adds answers by providing the content, questionid, and submissionid (input of the addAnswer function).
+        '''A student adds answers by providing the content, questionid, and submissionid (input of the addAnswer function).
         It checks if the submissionid has an ongoing evaluation request. If so, changes will be ignored.'''
-        if select(EvaluationRequest).where(EvaluationRequest.submissionid) != NULL:
+        query = select(EvaluationRequest).where(EvaluationRequest.submissionid == submissionid)
+        result = self._engine.execute(query)
+        if not len(result.all()) == 0:
             print("Evaluation request has been requested for this submission. Answers will be not be added")
             pass
         else:
             with self.newSession() as ses:
-                an = Answers(Text = content, Question = questionid, Submission = submissionid)
+                an = Answers(content = content, questionId = questionid, submissionid = submissionid)
                 ses.add(an)
                 ses.commit()
                 return an.answerid
 
     def commitSubmission(self, submissionid):
-        '''The teacher adds commited submissions by providing the submissionid (input of the commitSubmission function).'''
+        '''A student commits a submission by providing the submissionid (input of the commitSubmission function).'''
         ## TO DO: use submissionid > assignmentid > universityid to check if the universityid already has a submissionid to their name ##
         # if select(Student).where()
         #     pass
         # else:
         with self.newSession() as ses:
-            cs = EvaluationRequest(Submission = submissionid)
+            cs = EvaluationRequest(submissionid = submissionid)
             ses.add(cs)
             ses.commit()
             print("New submission")
@@ -97,7 +105,7 @@ class Gradedb:
     def newEvaluation(self, requestid):
         '''The teacher adds new evaluation by providing the requestid (input of the newEvaluation function).'''
         with self.newSession() as ses:
-            e = Evaluation(EvaluationRequest = requestid)
+            e = Evaluation(requestid = requestid)
             ses.add(e)
             ses.commit()
             return e.evaluationid
@@ -105,14 +113,16 @@ class Gradedb:
     def addScore(self, score, evaluationid, answerid):
         '''The teacher adds scores by providing the score, evaluationid and answerid (input of the addScore function).
         If the evaluation is finished, any further scores added will be ignored'''
-        if select(EvaluationFinished).where(EvaluationFinished.evaluationid) != NULL:
+        q = select(EvaluationFinished).where(EvaluationFinished.evaluationid == evaluationid)
+        result = self._engine.execute(q)
+        if not len(result.all()) == 0:
             print("The evaluation is finalized, scores added to the evaluation will be ignored")
             pass
         elif score<1 or score>10:
             raise Exception("Score must be between 1 and 10")
         else:
             with self.newSession() as ses:
-                sc = Scores(Value = score, Evaluation = evaluationid, Answer = answerid)
+                sc = Scores(value = score, evaluationid = evaluationid, answerid = answerid)
                 ses.add(sc)
                 ses.commit()
                 return sc.scoreid
@@ -120,7 +130,7 @@ class Gradedb:
     def commitEvaluation(self, evaluationid):
         '''The teacher adds commited evaluations by providing the evaluationid (input of the commitEvaluation function).'''
         with self.newSession() as ses:
-            ev = EvaluationFinished(Evaluation=evaluationid)
+            ev = EvaluationFinished(evaluationid = evaluationid)
             ses.add(ev)
             ses.commit()
             print("Submission evaluated")
